daef5164fd1a238e0c92a36f3298c8cec0215406
Yimeng Ma <yima@unc.edu>
2023-09-17 10:10:15 -0400

    ....

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -12,19 +12,26 @@ secret_word_idx: int = str(secret_word[0])
 secret_pattern: str = ""
 
 
-if len(str(six_letter_guess)) != int(6):
+if len(str(six_letter_guess)) != len(str(secret_word)):
     while chances <= int(3):
-        if len(str(six_letter_guess)) != int(6):
+        if len(str(six_letter_guess)) != len(str(secret_word)):
             chances += 1
             guess_again: str = input("That was not 6 letters! Try again: ")
-    else: chances = int (3)
-    print("Not quite. Play again soon!")
-
-
-if len(str(six_letter_guess)) == int(6):
+        elif chances == int (3) and str(six_letter_guess) != str(secret_word):
+            print("Not quite. Play again soon!")
+if len(str(six_letter_guess)) == len(str(secret_word)):
     while check_idx < len(str(secret_word)):
         if str(six_letter_guess[check_idx]) == str(secret_word[check_idx]):  
-            secret_pattern = secret_pattern + GREEN_BOX       
+            secret_pattern = secret_pattern + GREEN_BOX      
+        elif str(six_letter_guess[check_idx]) != str(secret_word[check_idx]):
+            alt_idx: int = 0  
+            present: bool = str(six_letter_guess[alt_idx]) != str(secret_word[check_idx]) and False                
+            while present and alt_idx < len(str(secret_word)):
+                if str(six_letter_guess[alt_idx]) == str(secret_word[check_idx]):
+                    present = True
+                elif str(six_letter_guess[alt_idx]) != str(secret_word[check_idx]):
+                    alt_idx = alt_idx + 1
+            secret_pattern = secret_pattern + YELLOW_BOX
         else:
             secret_pattern = secret_pattern + WHITE_BOX
         check_idx = check_idx + 1  
@@ -34,14 +41,7 @@ if len(str(six_letter_guess)) == int(6):
     elif str(six_letter_guess) == secret_word:
         print("Woo! You got it!")
 
-alt_idx: int = 0  
-present: bool = str(six_letter_guess[alt_idx]) != str(secret_word[alt_idx]) and False                 
-while present and alt_idx < len(str(secret_word)):
-    if str(six_letter_guess[alt_idx]) == str(secret_word[alt_idx]):
-        present = True
-    elif str(six_letter_guess[alt_idx]) != str(secret_word[alt_idx]):
-        alt_idx = alt_idx + 1
-        secret_pattern = secret_pattern + YELLOW_BOX
+
 
 
 
