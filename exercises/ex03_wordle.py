0d7ebc6deed9f7eab41897f04a4ea2c78ee354e8
Yimeng Ma <yima@unc.edu>
2023-09-21 17:23:35 -0400

    .

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -4,9 +4,10 @@ __author__ = "730668363"
 def contains_char(search_for_char: str, char_check: str) -> str:
     assert len(char_check) == 1
     """Looks for the input of char in the declared word."""
-    search_for_char_idx: int = 0
-    if char_check == (search_for_char[search_for_char_idx]):
+    idx: int = 0
+    if char_check == (search_for_char[idx]):
         return True
     else:
         return False
-    
+
+print(contains_char("abc", "a"))
