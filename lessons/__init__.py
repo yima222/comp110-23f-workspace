1867a440ee237b434abd5820555505853d7d6f25
Yimeng Ma <yima@unc.edu>
2023-09-21 20:26:46 -0400

    ...

--- lessons/__init__.py
+++ lessons/__init__.py
@@ -2,13 +2,27 @@
 __author__ = "730668363"
 
 def contains_char(search_for_char: str, char_check: str) -> str:
-    assert len(char_check) == 1
     """Looks for the input of char in the declared word."""
+    assert len(char_check) == 1
     idx: int = 0
     while idx < len(search_for_char):
+        alt_idx: int = 0
         if char_check == (search_for_char[idx]):
             return True
-    if char_check != (search_for_char[idx]):
+        idx = idx + 1
+    if char_check != (search_for_char[alt_idx]):
         return False
-    idx = idx + 1
-print(contains_char("abc", "z"))
+    alt_idx = alt_idx + 1
+
+def emojified(guess: str, secret_word: str) -> str:
+    """Checks for the characters of the secret word in the guess inputted."""
+    assert len(guess) == len(secret_word)
+    WHITE_BOX: str = "\U00002B1C"
+    GREEN_BOX: str = "\U0001F7E9"
+    YELLOW_BOX: str = "\U0001F7E8"
+    secret_pattern: str = ""
+    if contains_char is True:
+        secret_pattern = secret_pattern + YELLOW_BOX
+    elif contains_char is False:
+        secret_pattern = secret_pattern + WHITE_BOX
+    print(secret_pattern)
