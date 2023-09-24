4c955af3b1c8a7c790cc156b852f049f724b6b52
Yimeng Ma <yima@unc.edu>
2023-09-23 22:08:04 -0400

    ....

--- lessons/__init__.py
+++ lessons/__init__.py
@@ -3,19 +3,24 @@ __author__ = "730668363"
 
 def contains_char(search_for_char: str, char_check: str) -> str:
     """Looks for the input of char in the declared word."""
+    # Length of the input must be one character
     assert len(char_check) == 1
     idx: int = 0
     while idx < len(search_for_char):
         alt_idx: int = 0
+        # Tell them True if the guessed character is in the word 
+        # (this doesn't indicate the index at which the guessed char is in the word)
         if char_check == (search_for_char[idx]):
             return True
         idx = idx + 1
+    # Tell them False if the guessed character is not in the word
     if char_check != (search_for_char[alt_idx]):
         return False
     alt_idx = alt_idx + 1
 
 def emojified(guess: str, secret_word: str) -> str:
     """Checks for the characters of the secret word in the guess inputted."""
+    # Length of the guess must be the same as the secret word
     assert len(guess) == len(secret_word)
     WHITE_BOX: str = "\U00002B1C"
     GREEN_BOX: str = "\U0001F7E9"
@@ -35,4 +40,18 @@ def emojified(guess: str, secret_word: str) -> str:
         check_idx = check_idx + 1  
     return(secret_pattern)
 
-print(emojified("hello", "world"))
+def input_guess(guess_exp_len: int) -> int:
+    """Prompts user for a correct length of guess for the secret word"""
+    guess: str = input(f"Enter a {guess_exp_len} character word: ")
+    while len(guess) != guess_exp_len:           
+        guess = input(f"That was not {guess_exp_len} chars! Try again: ")
+    # The correct length guess is returned after exiting the above while loop
+    return guess
+
+def main() -> None:
+    """The entrypoint of the program and main game loop"""
+    # Code for the wordle game
+
+
+
+
