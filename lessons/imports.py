86c044149afe784657158118b9cc717cad4307f3
Yimeng Ma <yima@unc.edu>
2023-09-28 14:20:45 -0400

    ...

---
+++ lessons/imports.py
@@ -0,0 +1,5 @@
+"""Practice importing from other modules"""
+
+from lessons import my_functions
+
+print(my_functions.addition(1,2))
---
+++ lessons/my_functions.py
@@ -0,0 +1,4 @@
+"""Things I'll be importing"""
+
+def addition(x: int, y: int):
+    return x + y
