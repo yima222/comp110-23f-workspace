6f2cbb3c12e7defe337c3d82460ad6fbdd1849f6
Yimeng Ma <yima@unc.edu>
2023-10-05 23:24:12 -0400

    is_equal

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -4,10 +4,25 @@ __author__ = "730668363"
 def all(int_list: list[int], input: int) -> bool:
     check_idx: int = 0
     int_check: bool = False
-    while check_idx < len(int_list):
+    while (int_check is False) and (check_idx < len(int_list)):
         while input == int_list[check_idx]:
             check_idx += 1
         return True
-    if input != int_list[check_idx]:
-        return False
+       
 
+def max(int_list: list[int]) -> int:
+    max_idx: int = 0
+
+
+def is_equal(list1: list[int], list2: list[list]) -> bool:
+    list_idx: int = 0
+    equal: bool = False
+    while (equal is False) and (list_idx < (len(list1) and len(list2))):
+        if list1[list_idx] == list2[list_idx]:
+            equal is True
+            list_idx += 1
+        else:
+            return False
+    return True
+
+print(is_equal([1, 0, 1], [1, 0, 1]))
--- lessons/__init__.py
+++ lessons/__init__.py
@@ -1,6 +1,28 @@
-def f(x: int) -> int:
-    print(x)
-    x *= 2
-    return x
+"""Ex04- Building list functions to practice computational thinking."""
+__author__ = "730668363"
 
-y: int = f(3)
+def all(int_list: list[int], input: int) -> bool:
+    check_idx: int = 0
+    int_check: bool = False
+    while (int_check is False) and (check_idx < len(int_list)):
+        while input == int_list[check_idx]:
+            check_idx += 1
+        return True
+       
+
+def max(int_list: list[int]) -> int:
+    max_idx: int = 0
+
+
+def is_equal(list1: list[int], list2: list[list]) -> bool:
+    list_idx: int = 0
+    equal: bool = False
+    while (equal is False) and (list_idx < (len(list1) and len(list2))):
+        if list1[list_idx] == list2[list_idx]:
+            equal is True
+            list_idx += 1
+        else:
+            return False
+    return True
+
+print(is_equal([1, 0, 1], [1, 0, 1]))
