14e696eaefb707d1b9b38eaed0a45333fde88cd7
Yimeng Ma <yima@unc.edu>
2023-10-08 23:17:33 -0400

    YAY

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,12 +1,14 @@
 """Ex04- Building list functions to practice computational thinking."""
 __author__ = "730668363"
 
+
 def all(int_list: list[int], input: int) -> bool:
     check_idx: int = 0
     while (check_idx < len(int_list)):
         if input != int_list[check_idx]:
             return False
         elif input == int_list[check_idx]:
+            # Nested while loop to allow program to check all the numbers in the list
             while check_idx < len(int_list):
                 if input != int_list[check_idx]:
                     return False
@@ -16,7 +18,20 @@ def all(int_list: list[int], input: int) -> bool:
         
 
 def max(int_list: list[int]) -> int:
-    max_idx: int = 0
+    if len(int_list) == 0:
+        raise ValueError("max() arg is an empty List")
+    elif len(int_list) > 0:
+        max_idx: int = 0
+        # max_int allows program to update the number
+        # if the current number in the list is greater than
+        # the one before it
+        max_int: int = int(int_list[0])
+        while max_idx < len(int_list):
+            present_int: int = int(int_list[max_idx])
+            if present_int > max_int:
+                max_int = present_int
+            max_idx += 1
+        return max_int
 
 
 def is_equal(list1: list[int], list2: list[list]) -> bool:
@@ -29,5 +44,3 @@ def is_equal(list1: list[int], list2: list[list]) -> bool:
         else:
             return False
     return True
-
-print(all([1, 1, 1, 1], 1 ))
