e014c051d49c65a5ce1d267557dd421410126008
Yimeng Ma <yima@unc.edu>
2023-10-09 21:39:41 -0400

    ....

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -39,13 +39,16 @@ def max(int_list: list[int]) -> int:
 
 
 def is_equal(list1: list[int], list2: list[int]) -> bool:
-    """Searches through two lists to determine if all the elemenst are the same in both lists."""
-    list1_idx: int = 0
-    list2_idx: int = 0
-    while ((list1_idx < (len(list1)) or (list2_idx < len(list2)))):
-        if list1[list1_idx] != list2[list2_idx]:
+    """Searches through two lists to determine if all the elements are the same in both lists."""
+    list_idx: int = 0
+    match_int: bool = True
+    if len(list1) != len(list2):
+        return False
+    while (list_idx < len(list1)) and (list_idx < len(list2)) :
+        if list1[list_idx] != list2[list_idx]:
             return False
-        elif list1[list1_idx] == list2[list2_idx]:
-            list1_idx += 1
-            list2_idx += 1
-            return True
+        elif list1[list_idx] == list2[list_idx]:
+            list_idx += 1
+    return True
+
+print(is_equal([1, 2, 1], [1, 2]))
