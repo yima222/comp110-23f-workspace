460d9c15b66c159a296b307a52e85842180cb57e
Yimeng Ma <yima@unc.edu>
2023-10-16 22:16:19 -0400

    Almost done

--- exercises/turtle_project.py
+++ exercises/turtle_project.py
@@ -10,7 +10,7 @@ def main() -> None:
     """The entrypoint of my scene."""
     ertle: Turtle = Turtle()
     # Draws a sand colored rectangle as the beach and
-    # Draws a light blue colored rectngle as the ocean
+    # Draws a light blue colored rectangle as the ocean
     ertle.color(194, 178, 128)
     ertle.begin_fill( )
     draw_rectangle(ertle, -320, 0, 320)
@@ -25,21 +25,31 @@ def main() -> None:
     draw_square(ertle, 230, 300, 80)
     ertle.end_fill()
     # Draws triangles to represent sun rays
-    ertle.color(253, 184, 19)
+    i: int = 0
+    x: int = 0
+    y: int = 0
+    while (i < 3):
+        ertle.color(253, 184, 19)
+        ertle.begin_fill()
+        draw_triangle(ertle, 160 + x, 260 + y, 30)
+        ertle.end_fill()
+        x += 40
+        y -= 60
+        i += 1
+    # Draw a sideways trapezoid with an upside down trapezoid to represent the fans of boats on the ocean
+    ertle.color(173, 106, 250)
     ertle.begin_fill()
-    draw_triangle(ertle, 180, 250, 30)
+    draw_sideway_trapezoid(ertle, -300, 50, 70)
     ertle.end_fill()
-    ertle.color(253, 184, 19)
-    ertle.begin_fill()
-    draw_triangle(ertle, 190, 190, 30)
+    ertle.color(100, 100, 200)
+    ertle.begin_fill( )
+    draw_updown_trapezoid(ertle, -310, 50, 60 )
     ertle.end_fill()
-    ertle.color(253, 184, 19)
-    ertle.begin_fill()
-    draw_triangle(ertle, 240, 180, 30)
-    ertle.end_fill()
-    ertle.color(253, 184, 19)
-    ertle.begin_fill()
-    draw_triangle(ertle, 280, 180, 30)
+    # Trapezoid to represent curve of ocean
+    ertle.pencolor(173, 106, 250)
+    ertle.color(173, 216, 230)
+    ertle.begin_fill( )
+    draw_updown_trapezoid(ertle, -320, 0, 640 )
     ertle.end_fill()
     # Declare your Turtle variables here
     # Call the procedures you define and pass your Turtles as an argument
@@ -80,11 +90,11 @@ def draw_square(b_turtle: Turtle, x: float, y: float, width: float) -> None:
 
 def draw_triangle(c_turtle: Turtle, x: float, y: float, width: float) -> None:
     """Draw a wave shape of the given width whose top left corner is located at x, y."""
+    c_turtle.speed(100)
     c_turtle.penup()
     c_turtle.goto(x, y)
     c_turtle.setheading(0.0)
     c_turtle.pendown()
-    
     i: int = 0
     while (i < 3):
         c_turtle.forward(width)
@@ -92,7 +102,48 @@ def draw_triangle(c_turtle: Turtle, x: float, y: float, width: float) -> None:
         i = i + 1
     
 
-# Create a blue trapezoid draw function to generate a curve in the sand
+def draw_sideway_trapezoid(d_turtle: Turtle, x: float, y: float, width: float) -> None:
+    """Draw a trapezoid of the given width whose one acute angle is located at x, y."""
+    d_turtle.speed(100)
+    d_turtle.penup()
+    d_turtle.goto(x, y)
+    d_turtle.setheading(0.0)
+    d_turtle.pendown()
+    width = width/2
+    d_turtle.forward(width)
+    d_turtle.left(60)
+    d_turtle.forward(width)
+    d_turtle.left(60)
+    d_turtle.forward(width)
+    width = width*2
+    d_turtle.left(120)
+    d_turtle.forward(width)
+
+
+def draw_updown_trapezoid(e_turtle: Turtle, x: float, y: float, width: float) -> None:
+    """Draw a trapezoid of the given width whose one acute angle is located at x, y."""
+    e_turtle.speed(100)
+    e_turtle.penup()
+    e_turtle.goto(x, y)
+    e_turtle.setheading(0.0)
+    e_turtle.pendown()
+    e_turtle.forward(width)
+    e_turtle.right(120)
+    width = width/2
+    e_turtle.forward(width)
+    e_turtle.right(60)
+    e_turtle.forward(width)
+    e_turtle.right(60)
+    e_turtle.forward(width)
+
+def draw_line(f_turtle: Turtle, x: float, y: float, width: float) -> None:
+    """Draw lines whose left point starts at x, y."""
+    f_turtle.speed(100)
+    f_turtle.penup()
+    f_turtle.goto(x, y)
+    f_turtle.setheading(0.0)
+    f_turtle.pendown()
+    f_turtle.forward(width)
 
 # Use the __name__ is "__main__" idiom shown in class
 if __name__ == "__main__":
