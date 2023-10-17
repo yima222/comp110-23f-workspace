3f6649a59bf87c7735a592be57bb4d6566e9eb4f
Yimeng Ma <yima@unc.edu>
2023-10-17 11:57:05 -0400

    ...

---
+++ lessons/sum.py
@@ -0,0 +1,28 @@
+"""Summing the elements of a list using different loops."""
+__author__ = "730668363"
+
+def w_sum(vals: list[float]) -> float:
+    """Adds values in a list using the while loop."""
+    val_idx: int = 0
+    sum_value: float = 0.0
+    while (val_idx < len(vals)):
+        sum_value += vals[val_idx]
+        val_idx += 1
+    return sum_value
+
+
+def f_sum(vals: list[float]) -> float:
+    """Adds values in a list using the for...in... loop."""
+    total_sum: float = 0.0
+    for numbers in vals:
+        total_sum += numbers
+    return total_sum
+
+
+def f_range_sum(vals: list[float]) -> float:
+    """Adds values in a list uisng the for...in range(...). """
+    sum_all: float = 0.0
+    for idx in range(0, len(vals)):
+        values: float = vals[idx]
+        sum_all += values
+    return sum_all
---
+++ tools/__init__.py
@@ -0,0 +1,5 @@
+names: list[str] = ["Alyssa", "Janet", "Vrinda"]
+
+for idx in range(0, 3):
+    elem: str = names[idx]
+    print(f"{idx}:{elem}")
