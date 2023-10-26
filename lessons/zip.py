a63cc74174ce4c10f77960691a2d7991f91da577
Yimeng Ma <yima@unc.edu>
2023-10-25 23:46:49 -0400

    ...

--- lessons/zip.py
+++ lessons/zip.py
@@ -2,12 +2,14 @@
 __author__ = "730668363"
 
 def zip(l1: list[str], l2: list[int]) -> dict[str, int]:
-    if len(l1) != len(l2):
-        zip == ()
-    else:
-        zip: dict[str, int] = {l1: l2}
-        for key in zip:
-            return (zip)
+    final_zip = dict()
+    if len(l1) != len(l2) or (l1 or l2 == ()):
+        return final_zip
+    elif len(l1) == len(l2):
+        for idx in range(len(l2)):
+            values: int = l1[idx]
+            final_zip = l1[values]
+        return final_zip
 
 print(zip(["Happy", "Tuesday"], [1, 2]))
     
