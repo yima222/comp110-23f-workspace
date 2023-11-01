687356b25173d4945abd2f49c55fa7d755b7322a
Yimeng Ma <yima@unc.edu>
2023-10-31 20:41:49 -0400

    ....

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -1,9 +1,10 @@
 """EX06: Practice with dictionary functions."""
 __author__ = "730668363"
 
+
 def invert(dictionary: dict[str, str]) -> dict[str, str]:
     """Given an input dictionary, ivert will reverse the order of the keys and values."""
-    inverted_dicts = dict()
+    inverted_dicts: dict[str, str] = dict()
     for string_keys in dictionary:
         string_values = dictionary[string_keys]
         if string_values in inverted_dicts:
@@ -14,46 +15,53 @@ def invert(dictionary: dict[str, str]) -> dict[str, str]:
 
 def favorite_color(dictionary: dict[str, str]) -> str:
     """Given a dict of names and colors, returns the color that appears most frequently."""
-    pop_color = ()
+    pop_color: str = ""
+    high_count: int = 0
+    # Can't figure out the current_count: int = 0, so what if I declared a dictionary to keep track of the color count like in the count function.
+    current_count: dict[str, int] = dict()
     for names in dictionary:
         color = dictionary[names]
-        if color in pop_color:
-            pop_color[color] += 1
+        if color in current_count:
+            current_count[color] += 1
         else:
-            pop_color[color] = 1
-print(favorite_color({"Marc": "yellow", "Ezri": "blue", "Kris": "blue"}))
+            current_count[color] = 1
+        if current_count[color] > high_count:
+            high_count = current_count[color]
+            pop_color = color
+    return pop_color
+
 
 def count(org_list: list[str]) -> dict[str, int]:
-    org_dictionary = dict
-    idx: int = 0
-    current_value: str = org_list[idx]
-    cv_count: int = 0
-    while idx <= len(org_list):
-        if current_value in org_list:
-            org_dictionary[org_list] = current_value
-            cv_count += 1
-            idx += 1
+    """Given a list of str, returns a dict regarding the count of the keys from list."""
+    org_dictionary: dict[str, int] = dict()
+    current_value_count: int = 0
+    for values in org_list:
+        if values in org_dictionary:
+            current_value_count += 1
+            org_dictionary[values] = current_value_count
         else:
-            cv_count += 1
-            org_dictionary[current_value] = cv_count
-        return org_dictionary
-#print(count(["hello", "hello", "bye"]))
+            current_value_count = 1
+            org_dictionary[values] = current_value_count
+    return org_dictionary
 
 
-#def alphabetizer(a_list: list[str]) -> dict[str, list[str]]:
+def alphabetizer(a_list: list[str]) -> dict[str, list[str]]:
+    """Takes a list of str and sort it in a dict alphabetically."""
+    ordered_dict: dict[str, list[str]] = dict()
+    for word in a_list:
+        idx: int = 0
+        letter1 = word[0].lower()
+        if letter1 in ordered_dict and (word[idx] == letter1):
+            ordered_dict[letter1] += word
+        else:
+            ordered_dict[letter1] = [word]
+    return ordered_dict
 
 
 def update_attendance(presence: dict[str, list[str]], week_day: str, student: str) -> dict[str, list[str]]:
-    if student and week_day in presence:
-        presence[week_day] = student
+    """Adds students to week_day given a presence dict through mutating the dict."""
+    if week_day in presence:
+        presence[week_day] += [student]
+    else:
+        presence[week_day] = [student]
     return presence
-    
-    
-
-attendance_log: dict = {"Monday": ["Vrinda", "Kaleb"], "Tuesday": ["Alyssa"]}
-update_attendance(attendance_log, "Tuesday" , "Vrinda")
-update_attendance(attendance_log, "Wednesday" , "Kaleb")
-print(attendance_log)
-
-
-
--- lessons/ex.py
+++ lessons/ex.py
@@ -1,5 +1,8 @@
-"""Review for Quiz demos."""
+def shrink(nums: list[str]) -> list[int]:
+    shrunk_list: list[int] = list()
+    for number in nums:
+        if (number % 2 == 0) and (number < 7):
+            shrunk_list.append(number)
+    return shrunk_list
 
-grocery_list: list[str] = ["bananas", "milk", "bread"]
-len(grocery_list)
-print(len(grocery_list))
+print(shrink([4, 5, 6, 8]))
