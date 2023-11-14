f683e44501434b175ca61cbdb438a75143774a2a
Yimeng Ma <yima@unc.edu>
2023-11-13 21:25:52 -0500

    YAYYY

--- exercises/ex08/river.py
+++ exercises/ex08/river.py
@@ -24,43 +24,38 @@ class River:
 
     def check_ages(self):
         """Checks the ages of the bears and fishes and removes them accordingly."""
-        bear_count: list[Bear] = self.bears
-        for bears in bear_count:
-            if Bear.age > 5:
-                bear_count.pop(Bear())
-            self.bears = bear_count
-        fish_count: list[Fish] = self.fish
-        for fishes in fish_count:
-            if Fish.age > 3:
-                fish_count.pop(Fish())
-            self.fish = fish_count
+        surviving_bears: list[Bear] = list()
+        for bear in self.bears:
+            if bear.age <= 5:
+                surviving_bears.append(bear)
+        self.bears = surviving_bears
+        fish_count: list[Fish] = list()
+        for fishes in self.fish:
+            if fishes.age <= 3:
+                fish_count.append(fishes)
+        self.fish = fish_count
         return None
     
     def remove_fish(self, amount: int):
         """Removes the number of fish equal to amount."""
-        remove_num: int = 0
-        fish_idx: int = 0
-        while remove_num <= amount:
-            (self.fish[fish_idx]).pop(Fish())
-            remove_num += 1
+        for fish in range(0, amount):
+            self.fish.pop(0)
         return None
 
     def bears_eating(self):
-        """Removes the same numbe rof fish as being eaten by the bear."""
-        eat_fish: int = 0
-        while eat_fish < 3:
-            if len(self.fish) <= 5:
-                self.remove_fish(1)
-                Bear.eat(1)
-                eat_fish += 1
+        """Removes the same numberof fish as being eaten by the bear."""
+        for bears in self.bears:
+            if len(self.fish) >= 5:
+                self.remove_fish(3)
+                bears.eat(3)
         return None
     
     def check_hunger(self):
         """Checks the hunger_score of bears and removes them if < 0."""
-        bears_count: list[Bear] = self.bears
-        for bears in bears_count:
-            if Bear.hunger_score < 0:
-                bears_count.pop(Bear)
+        bears_count: list[Bear] = list()
+        for bears in self.bears:
+            if bears.hunger_score >= 0:
+                bears_count.append(bears)
         self.bears = bears_count
         return None
         
@@ -118,8 +113,9 @@ class River:
 
     def one_river_week(self):
         """Simulates the river populations of bears and fishes in one week."""
-        week_idx: int = 0
-        while week_idx < 7:
+        day_idx: int = 0
+        while day_idx < 7:
             self.one_river_day()
-            week_idx += 1
+            day_idx += 1
+        
             
