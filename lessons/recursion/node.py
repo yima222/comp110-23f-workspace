00e9e1b9dd083c86f7e6c438fbc6000a7b0d43d4
abyrnes1 <abyrnes1@live.unc.edu>
2023-11-15 14:30:37 -0500

    Added cq for recursion

--- lessons/recursion/node.py
+++ lessons/recursion/node.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 class Node:
+<<<<<<< HEAD
 
     data: int
     next: Node | None
@@ -15,3 +16,31 @@ class Node:
             return str(self.data)
         else:    
             return str(self.data) + " -> " + self.next.pretty_list()
+=======
+    """My Node class for linked lists."""
+    
+    data: int
+    next: Node | None
+    
+    def __init__(self, data: int, next: Node | None):
+        """Construct Node."""
+        self.data = data
+        self. next = next
+        
+    def __str__(self) -> str:
+        """Produce a string visualization of the linked list."""
+        if self.next is None:
+            # base case (where it ends!)
+            return f"{self.data} -> None"
+        else:
+            return f"{self.data} -> {self.next}"
+        
+    def head(self):
+        return None
+    
+    def tail(self):
+        return None
+    
+    def last(self):
+        return None
+>>>>>>> db75118 (Added cq for recursion)
